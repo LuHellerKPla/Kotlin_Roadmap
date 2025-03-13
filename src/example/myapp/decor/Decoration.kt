9cf2e43426db5039544df3ceee1b6b5fd77ddb16
LuizaHellerKPla <luiza.pla@poatek.com>
2025-03-13 13:42:12 -0300

    feat: section 'Create a data class': - Step 2: Use destructuring

--- src/example/myapp/decor/Decoration.kt
+++ src/example/myapp/decor/Decoration.kt
@@ -16,8 +16,25 @@ fun makeDecorations() {
     println("---")
     println(decoration1.equals(decoration2))
     println(decoration3.equals(decoration2))
+    println("---")
 }
 
 fun main(){
-    makeDecorations()
+    makeDecoration2()
+}
+
+data class Decoration2(val rocks: String, val wood: String, val diver: String) {
+
+}
+fun makeDecoration2() {
+    println("---")
+    val d5 = Decoration2("crystal", "wood", "diver")
+    println(d5)
+
+    // assign all properties to variables
+    val (rock, _, diver) = d5
+    println(rock)
+//    println(wood)
+    println(diver)
+    println("---")
 }

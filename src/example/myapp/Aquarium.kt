98c86708e5b2d44667f1b71a3d2146b82fa829a7
LuizaHellerKPla <luiza.pla@poatek.com>
2025-03-13 09:45:00 -0300

    feat: section 'Add class constructors': - Step 1: Create a constructor - Step 2: Add init blocks - Step 3: Learn about secondary constructors

--- src/example/myapp/Aquarium.kt
+++ src/example/myapp/Aquarium.kt
@@ -1,9 +1,23 @@
 package example.myapp
 
-class Aquarium {
-    var width: Int = 20
-    var height: Int = 40
-    var length: Int = 100
+class Aquarium (var length: Int = 100, var height: Int = 40, var width: Int = 20) {
+
+
+    init {
+        println("Aquarium initializing")
+    }
+    init {
+        // 1 liter = 1000 cm^3
+        println("Volume: ${width * length * height / 1000}L ")
+    }
+
+    constructor(numberOfFish: Int): this() {
+        // 2000 cmˆ3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+
+        // calculate height needed
+        height = (tank / (length * width)).toInt()
+    }
 
     fun printSize() {
         println(
--- src/example/myapp/main.kt
+++ src/example/myapp/main.kt
@@ -2,10 +2,23 @@ package example.myapp
 
 
 fun buildAquarium() {
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
-    myAquarium.height = 60
-    myAquarium.printSize()
+    val aquarium1 = Aquarium()
+    aquarium1.printSize()
+
+    val aquarium2 = Aquarium(width = 25)
+    aquarium2.printSize()
+
+    val aquarium3 = Aquarium(height = 35, length = 110)
+    aquarium3.printSize()
+
+    val aquarium4 = Aquarium(width = 10, height = 75, length = 150)
+    aquarium4.printSize()
+
+    // volume will print twice, once by the init block in the primary constructor before the second is executed, and once by the code in buildAquarium()
+    println("-----")
+    val aquarium6 = Aquarium(numberOfFish = 29)
+    aquarium6.printSize()
+    println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000}L")
 }
 
 fun main(){

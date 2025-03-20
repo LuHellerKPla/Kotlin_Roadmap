83594babbe5c5e9ec753b9a03f3ae3df9ae20b7e
LuizaHellerKPla <luiza.pla@poatek.com>
2025-03-20 09:33:02 -0300

    feat: Lists

--- src/Main.kt
+++ src/Main.kt
@@ -1,29 +1,9 @@
 fun sayHelloTwoParams(greeting: String, itemToGreet: String) = println("$greeting $itemToGreet")
 
 fun main() {
-    val interestingThings = arrayOf("Kolin", "Programming", "Comic Books")
-    println(interestingThings.size)
-    println(interestingThings[0])
-    println(interestingThings.get(0))
-    println("-----")
-
-    // Iterating Over Collections
-    for (interestingThing in interestingThings) {
+    // Lists
+    val interestingThings = listOf("Kotlin", "Programming", "Comic Books")
+    interestingThings.forEach{ interestingThing ->
         println(interestingThing)
-    }
-    println("-----")
-    interestingThings.forEach {
-        println(it)
-    }
-    println("-----")
-    // Using another name for it
-    interestingThings.forEach { interestingThing->
-        println(interestingThing)
-    }
-
-    println("-----")
-    // Pass the current index as well as the current string
-    interestingThings.forEachIndexed{index, interestingThing ->
-        println("$interestingThing is at index $index")
     }
 }

0ba17ff61fb225220ce654e7c7e7a3a57ab58658
LuizaHellerKPla <luiza.pla@poatek.com>
2025-03-20 08:32:40 -0300

    feat: Functions and String Templates

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,29 @@
-fun getGreeting(): String {
-    return "Hello Kotlin"
+//Function Parameters
+fun sayHello(itemToGreet: String) {
+    val msg = "Hello " + itemToGreet
+    println(msg)
 }
 
-//Single Expression Function
-fun getGreetingSEF() = "Hello Single Expression Function"
-
-fun sayHello() {
-    println(getGreeting())
+// String Templates
+fun sayHelloWithStringTemplate(itemToGreet: String) {
+    val msg = "Hello $itemToGreet"
+    println(msg)
+    println("Hello $itemToGreet")
 }
+
+// Single expression function
+fun sayHelloInSEF(itemToGreet: String) = println("Hello $itemToGreet")
+
+// Take in two parameters
+fun sayHelloTwoParams(greeting: String, itemToGreet: String) = println("$greeting $itemToGreet")
 
 fun main() {
-    println("Hello World")
-    println(getGreeting())
-    sayHello()
-    println(getGreetingSEF())
+    sayHello("Kotlin")
+    sayHello("Lion")
+
+    sayHelloWithStringTemplate("String template")
+
+    sayHelloInSEF("whale")
+
+    sayHelloTwoParams("Hey", "Steve")
 }

deab3c41b8f0ad0a1fb2143d91802c938357b61e
LuizaHellerKPla <luiza.pla@poatek.com>
2025-03-20 09:40:20 -0300

    feat: Map and mutable

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,13 @@
 fun sayHelloTwoParams(greeting: String, itemToGreet: String) = println("$greeting $itemToGreet")
 
 fun main() {
-    // Lists
+    // Map
+    // Take in pairs -> simple wrapper class containing two values -> key | value
     val interestingThings = listOf("Kotlin", "Programming", "Comic Books")
-    interestingThings.forEach{ interestingThing ->
-        println(interestingThing)
-    }
+    val map = mapOf(1 to "a", 2 to "b", 3 to "c")
+    map.forEach { key, value -> println("$key -> $value") }
+
+    // Mutable List
+    val mutableInterestingThings = mutableListOf("Kotlin", "Programming", "Comic Books")
+    mutableInterestingThings.add("Dogs")
 }
